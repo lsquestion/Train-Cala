fdb8b17785af18eb4523609b23a5a1f2db97f87d
“liangsq” <971852457@qq.com>
2018-09-25 18:14:17 +0800

    'comment'

--- calc_func.py
+++ calc_func.py
@@ -1,4 +1,4 @@
-def Trac_Chara(Trac_kN,speed,Cons_Power_Point,Nature_Point):
+def Trac_Chara(Trac_kN,speed,Cons_Power_Point,Nature_Point):#牵引特性曲线函数
 	Trac_Cons_power=Trac_kN*Cons_Power_Point
 	Trac_Nature_power=Trac_Cons_power*Nature_Point
 	if speed<=Cons_Power_Point:
