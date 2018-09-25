ede89c62f685c3f3f0e300fa73fda20244059bd9
lsquestion <971852457@qq.com>
2018-09-25 23:56:56 +0800

    wan shan qian yin te xing han shu

--- calc_func.py
+++ calc_func.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 def Trac_Chara(Trac_kN,speed,Cons_Power_Point,Nature_Point):#牵引特性曲线函数
 	Trac_Cons_power=Trac_kN*Cons_Power_Point
 	Trac_Nature_power=Trac_Cons_power*Nature_Point
@@ -7,11 +10,21 @@ def Trac_Chara(Trac_kN,speed,Cons_Power_Point,Nature_Point):#牵引特性曲线�
 		Trac_kN=Trac_kN*Cons_Power_Point/speed
 	else:
 		Trac_kN=Trac_Cons_power*Nature_Point/speed/speed
-	return Trac_kN
+	Trac_kN=round(Trac_kN,2)
+	return [speed,Trac_kN]
 		
-		
-		
-		
-trac=Trac_Chara(350,50,36,60)
+def Trac_Point_voltage(Cons_Power_Point,Nature_Point,Trac_Voltage=1500,voltage=1000):
+	Cons_Power_Point=voltage/Trac_Voltage*Cons_Power_Point
+	Nature_Point=voltage/Trac_Voltage*Nature_Point
 
-print(trac)
+	return Cons_Power_Point,Nature_Point
+
+
+
+
+		
+trac=Trac_Chara(350,51,36,60)
+
+point1,point2=Trac_Point_voltage(30,60)
+
+print(point1,point2)
