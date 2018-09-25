d40947ee6f69dbd1d19d31cfb9cec39a5bd59e4e
lsquestion <971852457@qq.com>
2018-09-26 00:01:26 +0800

    not end

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
 from parameter import *
+from calc_func import *
+import numpy as np
 
 Mass_T_Weight=T_Car_Weight*T_Car_Num #拖车车重
 Mass_M_Weight=M_Car_Weight*M_Car_Num #动车车重
@@ -11,23 +15,16 @@ Mass_Weight_AW1=Mass_Weight_AW0+(T_Car_Pa_AW1*T_Car_Num+M_Car_Pa_AW1*M_Car_Num)*
 Mass_Weight_AW2=Mass_Weight_AW0+(T_Car_Pa_AW2*T_Car_Num+M_Car_Pa_AW2*M_Car_Num)*Pa_Weight #列车AW2质量
 Mass_Weight_AW3=Mass_Weight_AW0+(T_Car_Pa_AW3*T_Car_Num+M_Car_Pa_AW3*M_Car_Num)*Pa_Weight #列车AW3质量
 
+Speed_list=list(np.arange(0,Speed_Max+Speed_Step,Speed_Step))
 
+Trac_chara_list=[]
 
-def Trac_Cha(Trac_kN,speed,Cons_Power_Point,Nature_Point):
-	Trac_Cons_power=
-	if speed<=Cons_Power_Point:
-		Trc_kN=Trc_kN
-	elif speed>Cons_Power_Point and speed<Nature_Point:
-		Trc_kN=Trc_kN*Cons_Power_Point/speed
-	else:
-		Trc_kN=Trc_kN*Nature_Point*Nature_Point/speed/speed
-		
-		
+for i in Speed_list:
+	Trac_chara_list.append(Trac_Chara(350,i,36,60))
 
 
 
-
-print(Mass_Weight_AW0,Mass_Weight_AW1,Mass_Weight_AW2,Mass_Weight_AW3,Mass_Rota)
+print(Trac_chara_list)
 
 
 
